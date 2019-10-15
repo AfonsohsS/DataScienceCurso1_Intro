62dad13deeb0d77350667136b3f48e1d04fc530a
AfonsohsS <afonsohs@gmail.com>
2019-10-14 21:49:19 -0300

    Aula 5: Continuando com visualização - Formatando graficos de categoria

--- TMDBMovies5000.py
+++ TMDBMovies5000.py
@@ -12,8 +12,6 @@ import matplotlib.pyplot as plt
 
 tmdb = pd.read_csv("tmdb_5000_movies.csv")
 
-tmdb.columns = ["Language", "Total"]
-
 tmdb.head()
 
 #Trabalhando com as variáveis do TMDB
@@ -67,8 +65,28 @@ plt.pie(dados['total'], labels = dados['idioma'])
 
 #Analisando os dados dos outros idiomas
 
-tmdb.query("original_language != 'en'").original_language.value_counts()
+totalPerOtherLanguages = tmdb.query("original_language != 'en'").original_language.value_counts()
 
 otherLanguages = tmdb.query("original_language != 'en'")
-sns.catplot(x = 'original_language', kind = 'count', data = otherLanguages)
+
+#Plotagem configurando o aspect ratio, ordem e palheta de cores
+
+sns.catplot(x = 'original_language', 
+            kind = 'count', 
+            data = otherLanguages, 
+            aspect = 2, 
+            palette= "BuGn_r", 
+            order = totalPerOtherLanguages.index)
+
+#Outros exemplos de plotagem
+
+sns.set(style="ticks")
+
+# Load the example dataset for Anscombe's quartet
+df = sns.load_dataset("anscombe")
+
+# Show the results of a linear regression within each dataset
+sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df,
+           col_wrap=2, ci=None, palette="muted", height=4,
+           scatter_kws={"s": 50, "alpha": 1})
 
