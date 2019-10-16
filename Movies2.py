e9f834fb491caa8ec4274eb25230ad6a27b91f0d
AfonsohsS <afonsohs@gmail.com>
2019-10-16 07:34:35 -0300

    Aula 6 - Finalizado exercícios com várias forma de visualizar os dados, além do desvio padrão.

--- Movies2.py
+++ Movies2.py
@@ -14,7 +14,7 @@ notas.head()
 notasToyStory = notas.query("filmeId==1")
 notasJumanji = notas.query("filmeId==2")
 
-#Use o len() para contar o número de ocorrências
+##Use o len() para contar o número de ocorrências
 print(len(notasToyStory), len(notasJumanji))
 
 #Imprimindo a média  e mediana das notas com interpolação de 2 casas decimais
@@ -29,9 +29,10 @@ import numpy as np
 filme1 = np.append(np.array([2.5] * 10), np.array([3.5] * 10))
 filme2 = np.append(np.array([5] * 10), np.array([1] * 10))
 
-#Extraindo a média e mediana
+#Extraindo a média mediana + desvio padrão
 print(filme1.mean(), filme2.mean())
 print(np.median(filme1), np.median(filme2))
+print(np.std(filme1), np.std(filme2))
 
 #Visualizando a distribuição dos valores com Seaborn
 sns.distplot(filme1)
@@ -41,4 +42,19 @@ sns.distplot(filme2)
 plt.hist(filme1)
 plt.hist(filme2)
 
+plt.boxplot([filme1, filme2])
+
 plt.boxplot([notasToyStory.nota, notasJumanji.nota])
+
+#Explorando os dados com o Seaborn
+sns.boxplot(x="filmeId",y="nota", data=notas.query("filmeId in [1, 2]"))
+
+sns.boxplot(x="filmeId",y="nota", data=notas.query("filmeId in [1, 2, 3, 4]"))
+
+#Verificando o desvio padrão
+deviationNotasToyStory = notasToyStory.nota.std()
+deviationNotasJumanji = notasJumanji.nota.std()
+
+print("Desvio padrão do Toy Story: %.2F" % deviationNotasToyStory)
+print("Desvio padrão do Jumanji: %.2F" % deviationNotasJumanji)
+
